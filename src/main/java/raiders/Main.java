875b40b2537f90c27e2adc4d6333f0e18a1fb1c6
KnightsWhoSayNi0 <62893792+KnightsWhoSayNi0@users.noreply.github.com>
2021-06-15 22:09:47 -0400

    renderer

--- src/main/java/raiders/Main.java
+++ src/main/java/raiders/Main.java
@@ -1,14 +1,29 @@
 package raiders;
 
+import engine.graphics.Mesh;
+import engine.graphics.Renderer;
+import engine.graphics.Vertex;
 import engine.io.Input;
 import engine.io.Window;
+import engine.math.Vector3f;
 import org.lwjgl.glfw.GLFW;
 
 public class Main implements Runnable {
 
     public Thread game;
     public Window window;
+    public Renderer renderer;
     public final int WIDTH = 1920, HEIGHT = 1080;
+
+    public Mesh mesh = new Mesh(new Vertex[] {
+            new Vertex(new Vector3f(-0.5f,  0.5f, 0.0f)),
+            new Vertex(new Vector3f(-0.5f, -0.5f, 0.0f)),
+            new Vertex(new Vector3f( 0.5f, -0.5f, 0.0f)),
+            new Vertex(new Vector3f( 0.5f,  0.5f, 0.0f))
+    }, new int[] {
+            0, 1, 2,
+            0, 3, 2
+    });
 
     public void start() {
         game = new Thread(this, "game");
@@ -18,8 +33,10 @@ public class Main implements Runnable {
     public void init() {
         System.out.println("Initializing Star Raiders");
         window = new Window(WIDTH, HEIGHT, "Star Raiders");
+        renderer = new Renderer();
         window.setBackgroundColor(0, 0, 0.1f);
         window.create();
+        mesh.create();
     }
 
     public void run() {
@@ -38,6 +55,7 @@ public class Main implements Runnable {
     }
 
     private void render() {
+        renderer.renderMesh(mesh);
         window.swapBuffers();
     }
 
